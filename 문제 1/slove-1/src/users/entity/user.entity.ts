16896648dd0f03e454ac0e91f45d1376c2975ec0
foo-val <tedxcdj@kakao.com>
2025-03-16 20:55:34 +0900

    [sloveAll] new commitments due to additions

--- 문제 1/slove-1/src/users/entity/user.entity.ts
+++ 문제 1/slove-1/src/users/entity/user.entity.ts
@@ -2,7 +2,7 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  PrimaryGeneratedColumn,
+  PrimaryGeneratedColumn
 } from 'typeorm';
 
 @Entity()
@@ -21,4 +21,13 @@ export class User {
 
   @CreateDateColumn()
   createdAt: Date;
+
+  /**  데이터 저장 전/후 처리를 할수 있습니다
+  @BeforeInsert()
+  async hashPassword() {
+    const salt = await bcrypt.genSalt(10);
+    if (!this.password) return;
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  */
 }
